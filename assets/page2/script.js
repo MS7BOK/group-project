03af567a0073cfbae449ff6799a2752fbb2f1aaa
yperera20 <buddhinperera@yahoo.com>
2023-10-05 00:10:33 +1100

    youtube-video-search-included

--- assets/page2/script.js
+++ assets/page2/script.js
@@ -1,10 +1,26 @@
-// Handle form submission
-document.getElementById('myForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-    
-    // Get the input value
-    var inputValue = document.getElementById('textInput').value;
-    
-    // Display the input value in an alert
-    alert('You entered: ' + inputValue);
+// Video search
+$(document).ready(function(){
+    var API_KEY = "AIzaSyDsy-oCrYD9qGw9tkzrSQ_Q1OX7QVU8oog";
+    var video = '';
+
+    $("#form").submit(function (event) {
+        event.preventDefault();
+        var search = $("#search").val();
+        videoSearch(API_KEY, search, 4);
+    });
+
+    function videoSearch(key, search, maxResults) {
+        // Clear previous content
+        $("#videos").empty();
+
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function(data) {
+            console.log(data);
+            data.items.forEach(item => {
+                video = `
+                <iframe width="250" height="250" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+                `;
+                $("#videos").append(video);
+            });
+        });
+    }
 });
