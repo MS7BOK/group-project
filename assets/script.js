a684c2acb88d33180e4f74ea4d08d89a963b0065
Haider Shahid <haidershahid3.16@live.com>
2023-10-05 18:38:29 +1100

    Add new feature

--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,68 @@
 const SPOONACULAR_ENDPOINT = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch";
-const SPOONACULAR_API_KEY = "Yf6fde6b800mshe44c4d3edfcf41ap1ee82cjsn03452dcc2b8b";
+//const url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch';
+const searchBtn= document.getElementById("search-button")
+const searchInput= document.getElementById("search-input")
+const recipeContainer= document.querySelector(".recipes-api")
+const recipes = [
+    {
+        title: "Spaghetti Carbonara",
+        ingredients: ["spaghetti", "egg", "bacon", "parmesan", "garlic"],
+        instructions: "..."
+    },
+]
 
-function searchRecipes() {
-    const query = document.getElementById('searchInput').value;
-    const url = `${SPOONACULAR_ENDPOINT}?apiKey=${SPOONACULAR_API_KEY}&query=${query}&addRecipeInformation=true&number=10`;
-
-    fetch(url, {
+async function returnRecipes(ingredients) {
+    const RecipeId = "1087629";
+    
+    const url= `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=${ingredients}&number=5&ignorePantry=true&ranking=1`;
+    const options = {
+        method: 'GET',
         headers: {
-            "X-RapidAPI-Key": SPOONACULAR_API_KEY // Ensure you send the API key in headers if required by the API
+            'X-RapidAPI-Key': 'f6fde6b800mshe44c4d3edfcf41ap1ee82cjsn03452dcc2b8b',
+            'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
         }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => displayResults(data.results))
-    .catch(error => console.error('Error fetching data:', error));
-}
+    };
 
-function displayResults(recipes) {
-    // your display logic here
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new Error("Failed to fetch the recipe.");
+    }
+
+    const data = await response.json();
+    console.log(data);
+    displayRecipe(data)
 }
-recipes.forEach(recipe => {
-        const recipeDiv = document.createElement('div');
-        recipeDiv.innerHTML = `
-            <h2>${recipe.title}</h2>
-            <img src="${recipe.image}" alt="${recipe.title}">
-            <p>${recipe.summary}</p>
-            <a href="${recipe.sourceUrl}" target="_blank">View Full Recipe</a>
-            <br>
-            <button onclick="showDetails(${recipe.id})">Show More Details</button>
-        `;
-        resultsDiv.appendChild(recipeDiv);
-    });
-}
+ function displayRecipe(data){
+
+    for (let i = 0; i < data.length; i++) {
+        
+        const card= document.createElement("div")
+        card.setAttribute("class", "card")
+        const cardHeader= document.createElement("div")
+        cardHeader.setAttribute("class", "card-header")
+        const cardBody= document.createElement("div")
+        cardBody.setAttribute("class", "card-body")
+        const img= document.createElement("img")
+        img.setAttribute("src", data[i].image)
+        const h2= document.createElement("h2")
+        const anchor= document.createElement("a")
+        anchor.setAttribute("href", "#")
+        anchor.setAttribute("value", data[i].title)
+        anchor.textContent=data[i].title
+        h2.append(anchor)
+        cardHeader.append(h2)
+        cardBody.append(img)
+        card.append(cardHeader, cardBody)
+        recipeContainer.append(card)
+    }
+ }
+
+searchBtn.addEventListener("click", ()=>{
+    const userIngredients= searchInput.value
+    returnRecipes(userIngredients)
+})
+
+recipeContainer.addEventListener("click", ()=>{
+    const title= this.event.target.textContent
+    console.log(title);
+})
